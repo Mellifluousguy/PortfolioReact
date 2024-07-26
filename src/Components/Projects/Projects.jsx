010b1b1e538c3a3cc929025815e645aa65e59470
Mohit Anilkumar Gupta <123147288+Mellifluousguy@users.noreply.github.com>
2024-07-26 16:10:49 +0530

    Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import mecode from "../Assets/Projects/mecode.png"
-import HomePage from "../Assets/Projects/Webpage.png"
-import Ecommerce from "../Assets/Projects/Ecommerce.png"
-import card from "../Assets/Projects/card.png"
-import { Link } from 'react-router-dom'
-import './style.css'
+import mecode from "../Assets/Projects/mecode.png";
+import HomePage from "../Assets/Projects/Webpage.png";
+import Ecommerce from "../Assets/Projects/Ecommerce.png";
+import card from "../Assets/Projects/card.png";
+import './style.css';
 
 const Feedback = () => {
   const CV = `https://drive.google.com/uc?export=download&id=1RsRLiDucNiyILlkMOp7f9GTzBjLPO8F6`;
   const projects = [
     {
       title: "Mecode",
-      description:
-        "A service-based website built using React and React Routing.",
+      description: "A service-based website built using React and React Routing.",
       link: "https://github.com/Mellifluousguy/MeCode_Hub/",
       image: mecode,
     },
@@ -34,33 +32,35 @@ const Feedback = () => {
       link: "https://github.com/Mellifluousguy/Card",
       image: card,
     }
-    
   ];
+  
   return (
     <div id="project">
       <div className="graphicsContainer">
         <div className="graphics">
-            <h1>My Projects</h1>
-            <div className="projects-list">
-              {projects.map((project, index) => (
-                <div key={index} className="project-card">
-                  <img src={project.image} alt={project.title} />
-                  <div className="project-content">
-                    <h2>{project.title}</h2>
-                    <p>{project.description}</p>
-                    <a
-                      href={project.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      View Project
-                    </a>
-                  </div>
+          <h1>My Projects</h1>
+          <div className="projects-list">
+            {projects.map((project, index) => (
+              <div key={index} className="project-card">
+                <img src={project.image} alt={project.title} />
+                <div className="project-content">
+                  <h2>{project.title}</h2>
+                  <p>{project.description}</p>
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View Project
+                  </a>
                 </div>
-              ))}
-            </div>
-          <Link to=`${CV}`><li>{'Download CV'}</li></Link>
+              </div>
+            ))}
           </div>
+          <a href={CV} target="_blank" rel="noopener noreferrer">
+            <li>{'Download CV'}</li>
+          </a>
+        </div>
       </div>
     </div>
   );
